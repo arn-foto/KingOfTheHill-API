4a0ac0fb530b95a4a3a6bd375dd69a86ad46c786
Anthony <foto.arn@gmail.com>
2023-02-14 23:36:49 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("yo yo yo")
+const mongoose = require("mongoose")
+
+mongoose.set("strictQuery", false)
+
+async function main(){await mongoose.connect("mongodb://localhost/testDB", ()=> {console.log("connected...")}, e=> console.log(e)) }
+
