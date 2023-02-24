d819ca36982bd4650acbb9eaa16a17f01b777bda
Anthony <foto.arn@gmail.com>
2023-02-23 18:37:20 -0800

    Add more attibutes to Character Schema

--- model/Character.js
+++ model/Character.js
@@ -1,13 +1,19 @@
 import mongoose from "mongoose";
 
 //import from mongoose
-const {Schema, model} =   mongoose;
+const { Schema, model } = mongoose;
 
 const characterSchema = new Schema({
-    firstName: String,
-    lastName:String,
-    age: Number,
-    city: String,
-    state: String,
-    occupation: String
-})
+  url: String,
+  firstName: String,
+  lastName: String,
+  age: Number,
+  gender: String,
+  city: String,
+  state: String,
+  occupation: String,
+});
+
+const Character = model("Character", characterSchema);
+
+export default Character;
