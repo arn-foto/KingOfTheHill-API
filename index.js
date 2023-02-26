d3bbfd157ceed6659835a6edc1f9932763d778b2
Anthony <foto.arn@gmail.com>
2023-02-25 20:37:06 -0800

    + Format express for mongoDB

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
-const app = express();
-
+require("dotenv").config();
 const PORT = process.env.PORT || 5000;
+const CONNECTION = process.env.CONNECTION;
 
-// require("dotenv").config();
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(CONNECTION, () => {
+    console.log("connected to database");
+  })
+  .then(() => {
+    const app = express();
 
-app.listen(PORT, () => {
-  console.log("Server is active");
-});
+    app.listen(PORT, () => {
+      console.log("Connected to port " + PORT);
+    });
+  });
